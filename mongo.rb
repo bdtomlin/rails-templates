b7f7f4f831944a01d80ea5edda4f4fad997b1115
Bryan Tomlin <bryan@Bryan-Tomlins-MacBook-Pro.local>
2010-06-26 13:07:34 -0500

    added mongo template

--- mongo.rb
+++ mongo.rb
@@ -6,9 +6,8 @@ run 'rm public/javascripts/*.js'
 run 'mkdir public/javascripts/jquery'
 run "curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jquery/jquery-1.4.2.min.js"
 run 'curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js'
-
-gem 'devise', '1.1.rc1'
-gem 'mysql'
+gem 'bson_ext'
+gem 'mongo_mapper'
 
 gem "rspec", "2.0.0.beta.11", :group => :test
 gem "rspec-rails",      ">= 2.0.0.beta.11", :group => :test
@@ -18,7 +17,6 @@ gem "autotest", :group => :test
 gem "autotest-rails", :group => :test
 gem "cucumber", :group => :test
 gem "cucumber-rails", :group => :test
-gem "database_cleaner", :group => :test
 gem "capybara", :group => :test
 
 run "bundle install"
