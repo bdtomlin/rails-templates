0cd9bc588c1a063a9eb026e3666d9facc85cec50
bdtomlin <bdtomlin@gmail.com>
2010-07-31 23:10:39 -0500

    changed to just mysql and added email_spec

--- rails3.rb
+++ rails3.rb
@@ -7,6 +7,7 @@ run 'mkdir public/javascripts/jquery'
 run "curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jquery/jquery-1.4.2.min.js"
 run 'curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js'
 
+gem 'mysql'
 gem "haml"
 gem "hpricot" # for turning devise views into haml
 gem "devise", ">=1.1.rc2"
@@ -19,53 +20,14 @@ gem "autotest-rails", :group => :test
 gem "capybara", :group => :test
 gem "cucumber-rails", :group => :test
 gem "launchy", :group => :test
+gem "email_spec", :group => :test
 
-db = ask("1 for Mongoid, 2 for MongoMapper, 3 for Active Record: ").to_i
+run "bundle install"
 
-if db == 1
-  gem "mongo", "1.0.4"
-  gem "bson_ext", "1.0.4"
-  gem "mongoid", "2.0.0.beta9"
+run "rails generate email_spec:steps"
+run 'rails g rspec:install'
+run 'rails g cucumber:install --capybara --rspec'
 
-  run "bundle install"
-
-  run 'rails g rspec:install'
-  run 'rails g cucumber:install --capybara --rspec --skip-database'
-
-  file 'features/support/env.custom.rb', %{
-  ENV["RAILS_ENV"] ||= "test"
-  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-
-  Before { Mongoid.master.collections.each { |c| c.remove } }
-  }
-  run "rails generate mongoid:config"
-
-elsif db == 2
-  gem "mongo", "1.0.4"
-  gem "bson_ext", "1.0.4"
-  gem 'mongo_mapper'
-
-  run "bundle install"
-
-  run 'rails g rspec:install'
-  run 'rails g cucumber:install --capybara --rspec --skip-database'
-
-  file 'features/support/env.custom.rb', %{
-  ENV["RAILS_ENV"] ||= "test"
-  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-
-  MongoMapper.database.collections.each { |c| c.remove }
-  }
-
-elsif db == 3
-  gem 'mysql'
-
-  run "bundle install"
-
-  run 'rails g rspec:install'
-  run 'rails g cucumber:install --capybara --rspec'
-
-end
 
 run 'rm app/views/layouts/application.html.erb'
 file "app/views/layouts/application.html.haml", %{
