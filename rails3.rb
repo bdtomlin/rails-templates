b3e50377e88804c90c29150b28430cbfd2d51d31
bdtomlin <bdtomlin@gmail.com>
2010-07-31 23:12:02 -0500

    removed mysql line since it's generated automatically with the -d switch

--- rails3.rb
+++ rails3.rb
@@ -7,7 +7,6 @@ run 'mkdir public/javascripts/jquery'
 run "curl -L http://code.jquery.com/jquery-1.4.2.min.js > public/javascripts/jquery/jquery-1.4.2.min.js"
 run 'curl -L http://github.com/rails/jquery-ujs/raw/master/src/rails.js > public/javascripts/rails.js'
 
-gem 'mysql'
 gem "haml"
 gem "hpricot" # for turning devise views into haml
 gem "devise", ">=1.1.rc2"
